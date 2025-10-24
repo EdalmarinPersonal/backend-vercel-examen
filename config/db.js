f0b0536493acf2a1b9460861c2fa35e4b813cb22
Edgar Marin <edalmarin@gmail.com>
2025-10-23 21:29:21 -0300

    actualizar vercel

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://admin1:kViaFasWt6Gv1pLJ@cluster0.bgaqtx9.mongodb.net/destinos");
+        await mongoose.connect("mongodb+srv://admin1:kViaFasWt6Gv1pLJ@cluster0.bgaqtx9.mongodb.net/destinos", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Se conecto a la BD");
     } catch (error) {
         console.log(error);
